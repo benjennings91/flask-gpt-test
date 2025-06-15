e27101fe9344e1c3b3aa73592b0cffa29aad78c0
benjennings91 <ben.r.jennings@googlemail.com>
2025-06-15 19:49:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask, session, render_template
 app = Flask(__name__)
 import os
+from forms import LoginForm
 PASSWORD = os.getenv("PASSWORD")
 app.secret_key = os.getenv("secret_key")
 
 @app.route('/')
 def index():
     from openai import OpenAI
-    client = OpenAI()
+    if session["authenticated"]:
+        client = OpenAI()
 
-    response = client.responses.create(
-        model="gpt-4.1",
-        input="Write a one-sentence description of AI."
-    )
+        response = client.responses.create(
+            model="gpt-4.1",
+            input="Write a one-sentence description of AI."
+        )
 
-    return response.output_text
+        return response.output_text
+    else:
+        return "Access Denied"
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
